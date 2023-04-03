45279de6570c86ee96a0b6dbf195035d0fa65e32
Bhone Sithu <bhonesithu1418@gmail.com>
2023-04-03 17:16:26 +0630

    added snow effect/music/animation

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 $(
-    ()=>{
+    () => {
+        let musicPlay = false;
+        let music = new Audio("img/Thingyan_Music(Cropped).mp3");
+        // $("body").click(() => {
+            
+        //     music.play();
+        //     musicPlay = true;
+        // })
+        $("#music-icon").click(()=>{
+           
+        
+        console.log("hi");
+            if(!musicPlay) {
+                music.play();
+                musicPlay = true;
+                $("#music-icon").attr("src" , "icons/audio-svgrepo-com.svg");
+            } 
+            else {
+                music.pause();
+                musicPlay = false;
+                $("#music-icon").attr("src" , "icons/mute-svgrepo-com.svg");
+            }
+        })
         let prayers = [
             "နှစ်ဟောင်းရှိ ပူပင်သောကများ ကင်းစင်လွင့်ပျောက်ကာ အေးချမ်းသာယာတဲ့ နှစ်သစ်ကို ပိုင်ဆိုင်နိုင်ကြပါစေ",
             "မင်္ဂလာရှိသော နှစ်သစ်အခါသမယ ဖြစ်ပါစေ",
@@ -11,7 +33,7 @@ $(
             "နှစ်သစ်မှာ စိတ်သစ် နဲ့ ရည်းစား အသစ်​လေး ရပါ​စေ",
             "သင်္ကြန်မိုး နဲ့ အတူ ​ရောဂါပိုး​တွေ ကင်းပြီး ​ပျော်စရာ ကျန်းမာ​သော နှစ်သစ်ဖြစ်ပါ​စေ",
             "နှစ်သစ်ကူး သင်္ကြန်အခါသမယမှသည် ကျန်းမာချမ်းသာ ဘေးကင်းကွာ၍ ကောင်းသောလိုအင်ဆန္ဒများ ပြည့်ဝကြပါစေ",
-            "အတာ ​ရေ နဲ့ အတူ အထာ ​လေး​တွေ ​ပေးနိုင်ပါ​စေ",
+            "အတာ ​ရေ နဲ့ အတူ အထာတွေ ​ပေးနိုင်ပါ​စေ",
             "မုန့်လုံး​ရေ​ပေါ်စားရင် ငြုပ်သီးစိမ်း ​ရှောင်နိုင်ပါ​စေ",
             "အတာ​ရေလို​အေး ပန်းပိ​တောက်လို ​မွှေး၍ ​​ကောင်းကျိုးမင်္ဂလာ အ​ပေါင်းနှင့် ပြည့်စုံကြပါ​စေ",
             "မြန်မာ နှစ်သစ်ကူး အတာသင်္ကြန်မှာ မင်္ဂလာ အ​ပေါင်းနှင့် ပြည့်စုံ၍ ​ပျော်ရွှင် ချမ်း​မြေ့ကြပါ​​စေ​ကြောင်း CMO IT Team မှ အ​ချော​လေးများက ဆု​​တောင်းပေးလိုက်ပါတယ်",
@@ -19,26 +41,28 @@ $(
             "ရက်မြတ်သင်္ကြန် အချိန်အခါသမယတွင် CMHL ဝန်ထမ်းမိသားစုများအားလုံး ကျန်းမာ ချမ်းသာ ရွှင်လန်း ချမ်းမြေ့ကြပါစေ"
 
         ];
-        let random = Math.floor(Math.random()* prayers.length);
+        let random = Math.floor(Math.random() * prayers.length);
         $("#pray-text").html(prayers[random]);
         $("#pray-container").hide();
         $("#thingyan-couple-outline").hide();
 
-        $("#overlay").click(()=>{
-            $("#pray-container").hide();
+        $("#overlay").click(() => {
+            
+            $("#pray-container").fadeOut();
             $("#thingyan-couple-outline").hide();
-        });
-        
-        $("#thingyan").click(()=>{
-            let randoms = Math.floor(Math.random()* prayers.length);
-        $("#pray-text").html(prayers[randoms]);
-            $("#pray-container").show();
-            $("#thingyan-couple-outline").show();
         });
 
-        $("#cross-icon").click(()=>{
-            $("#pray-container").hide();
-            $("#thingyan-couple-outline").hide();
+        $("#thingyan").click(() => {
+
+            let randoms = Math.floor(Math.random() * prayers.length);
+            $("#pray-text").html(prayers[randoms]);
+            $("#pray-container").fadeIn();
+            $("#thingyan-couple-outline").fadeIn();
+        });
+
+        $("#cross-icon").click(() => {
+            $("#pray-container").fadeOut();
+            $("#thingyan-couple-outline").fadeOut();
         })
     }
 )
